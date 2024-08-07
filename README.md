14e1311fb2c777afe1d034162023f61293a44a64
Juan Pedro Grille <jopegrille.jg@gmail.com>
2024-08-07 17:21:04 +0200

    Added README and gitignore files as well as the database connection

---
+++ README.md
@@ -0,0 +1,21 @@
+# Travel Blog Website
+
+This is a Web Application that was created as a capstone project for the course "The Complete 2024 Web Development Bootcamp". It's intention was to make a travel blog website so that I can upload a summary for every destination that I've traveled to. This webapp uses a localhost PostgreSQL database, it manages all the CRUD methods to manipulate data for the posts of the website.
+
+## Getting this App up and running
+1. Clone this repository
+2. Install server packages
+```
+cd YOUR_PATH/'Project - Blog Web App'
+npm install
+```
+3. Start server
+(`nodemon .\index.js`)
+
+The last lines of the output should be
+```
+[nodemon] starting `node .\index.js`
+Server is running on port 3000
+```
+
+You can now point your browser to http://localhost:3000/!
--- index.js
+++ index.js
@@ -1,66 +1,70 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
-let data = [
-  {
-    title: 'Exploring the Serene Beaches of Bali',
-    date: 'February 28, 2024',
-    summary: 'Bali, known as the Island of the Gods, is a destination that beckons travelers with its stunning beaches, rich culture, and lush landscapes. Join me as I recount my recent adventure exploring the serene beaches of Bali.',
-    img: '/images/bali.jpg'
-  },
-  {
-    title: 'A Safari Adventure in the Maasai Mara',
-    date: 'December 18, 2023',
-    summary: 'Nestled in the heart of Kenya, the Maasai Mara National Reserve is renowned for its breathtaking landscapes and abundant wildlife. Join me as I recount my unforgettable safari adventure in this iconic destination.',
-    img: '/images/maasai-mara.jpg'
-  },
-  {
-    title: 'Discovering the Charms of Coastal California',
-    date: 'November 10, 2023',
-    summary: 'California, with its diverse landscapes and vibrant culture, offers endless opportunities for exploration and adventure. Join me as I recount my journey along the stunning coast of the Golden State, from the iconic beaches of Southern California to the rugged shores of the North Coast.',
-    img: '/images/big_sur.JPG'
-  },
-  {
-    title: 'Exploring the Serene Beaches of Bali',
-    date: 'February 28, 2024',
-    summary: 'Bali, known as the Island of the Gods, is a destination that beckons travelers with its stunning beaches, rich culture, and lush landscapes. Join me as I recount my recent adventure exploring the serene beaches of Bali.',
-    img: '/images/bali.jpg'
-  },
-  {
-    title: 'A Safari Adventure in the Maasai Mara',
-    date: 'December 18, 2023',
-    summary: 'Nestled in the heart of Kenya, the Maasai Mara National Reserve is renowned for its breathtaking landscapes and abundant wildlife. Join me as I recount my unforgettable safari adventure in this iconic destination.',
-    img: '/images/maasai-mara.jpg'
-  },
-  {
-    title: 'Discovering the Charms of Coastal California',
-    date: 'November 10, 2023',
-    summary: 'California, with its diverse landscapes and vibrant culture, offers endless opportunities for exploration and adventure. Join me as I recount my journey along the stunning coast of the Golden State, from the iconic beaches of Southern California to the rugged shores of the North Coast.',
-    img: '/images/big_sur.JPG'
-  }
-  // {
-  //   title: 'Test Title',
-  //   date: '',
-  //   summary: '',
-  //   img: ''
-  // },
-]
+let data = [];
 
 const app = express();
 const port = 3000;
-var numberOfLetters = 0;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "travel_blog",
+  password: "1234",
+  port: 5432,
+});
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+let currentUserId = 1;
+let users = [];
+
+async function getPosts() {
+  const result = await db.query("SELECT * FROM posts ORDER BY id ASC");
+  let posts = [];
+  result.rows.forEach((post) => { posts.push(post) });
+  return posts;
+}
+
+async function getCurrentUser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
+
+app.get("/", async (req, res) => {
+  data = await getPosts();
+  console.log(data);
   res.render("index.ejs",
     {postsData: data}
   );
 });
 
-app.get("/newPost", (req, res) => {
-    res.render("post.ejs");
+app.get("/newPost", async (req, res) => {
+  const currentUser = await getCurrentUser();
+  res.render("post.ejs",
+    {user: currentUser}
+  );
+});
+
+app.post("/newPost", async (req, res) => {
+  const title = req.body["postTitle"];
+  const content = req.body["postContent"];
+  const currentUser = await getCurrentUser();
+  const today = 'CURRENT_TIMESTAMP';
+  const img = '/images/big_sur.JPG';
+
+  try {
+    await db.query(
+      "INSERT INTO posts (title, summary, date, img, user_id) VALUES ($1, $2, $3, $4, $5)",
+      [title, content, new Date(), img, currentUserId]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/about", (req, res) => {
